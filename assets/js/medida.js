88eceb9816694db8717154eae20ab10efa1055a4
alu0100819182 <alu0100819182@ull.edu.es>
2016-03-11 17:00:10 +0000

    Arreglos

--- assets/js/medida.js
+++ assets/js/medida.js
@@ -1,17 +1,30 @@
 function Medida(valor,tipo)
 {
-    this.valor = valor;
-    this.tipo = tipo;
+    //if(!tipo)
+    //{
+    //  exp = XRegExp('(?<val> [-+]?[0-9]+(\\.[0-9]+)?(?:e[+-]?[0-9]+)?) #val \n' +
+    //                    '(\\s*) \n' +
+    //                    '(?<tipo> [a-zA-Z]) #tipo \n','x');
+//
+    //  m = XRegExp.exec(valor, exp);
+    //  this.valor = m.val;
+    //  this.tipo = m.tipo;
+  //  }
+  //  else
+  //  {
+      this.valor = valor;
+      this.tipo = tipo;
+  //  }
 }
 
 Medida.match = function(valor) {
   xregexp = XRegExp('(?<val> [-+]?[0-9]+(\\.[0-9]+)?(?:e[+-]?[0-9]+)?) #val \n' +
                     '(\\s*) \n' +
-                    '(?<tipo> [a-z]) #tipo \n' +
+                    '(?<tipo> [a-zA-Z]) #tipo \n' +
                     '(\\s*) \n' +
                     '(?<to> (to)?) #to \n' +
                     '(\\s*) \n' +
-                    '(?<opt>  [a-z]) #opt','x');
+                    '(?<opt>  [a-zA-Z]) #opt','x');
   valor = XRegExp.exec(valor,xregexp);
   return valor;
 }
@@ -21,20 +34,18 @@ Medida.measures = {};
 Medida.convertir = function(valor) {
   var measures = Medida.measures;
   measures.c = Celsius;
+  measures.C = Celsius;
   measures.f = Farenheit;
+  measures.F = Farenheit;
   measures.k = Kelvin;
+  measures.K = Kelvin;
   var match = Medida.match(valor);
   if (match) {
     var numero = match.val;
         tipo   = match.tipo;
         destino = match.opt;
     try {
-      console.log(measures['c']);
-      console.log(measures['f']);
-      console.log(measures['k']);
-      console.log(measures[tipo] + "(" + numero + ")");
-      var source = new measures[tipo](numero);
-      console.log(source); // new Fahrenheit(32)
+      var source = new measures[tipo](numero);// new Fahrenheit(32)
       var target = "to"+measures[destino].name; // "toCelsius"
       return source[target]().toFixed(2) + " "+measures[destino].name; // "0 Celsius"
     }
